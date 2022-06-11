d477f6deedb82aa346744253982686b209bf1b1d
PiotrZettt <piotrzielinski@o2.pl>
2022-06-11 14:20:20 +0100

    Initial Commit

--- CryptoPortfolio/settings.py
+++ CryptoPortfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+jei2hv7&fmz)kpr%%urpf&hn(3-t)js$ng1w4qieper+q%(#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
